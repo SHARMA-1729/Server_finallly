73e8b9893cc41d8b0957a6a39aa4baf44018ab71
Saurav Sharma <129518514+SHARMA-1729@users.noreply.github.com>
2024-10-06 00:52:36 +0530

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`,)
+        const connectionInstance = await mongoose.connect(`mongodb+srv://saurav:saurav109@cluster0.tpozm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/dashboard`,)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
