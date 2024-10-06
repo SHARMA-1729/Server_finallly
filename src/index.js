ccbc8805137a3e7e356556016c3fcbf3dd11ca0b
Saurav Sharma <129518514+SHARMA-1729@users.noreply.github.com>
2024-10-06 18:31:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ dotenv.config({
 
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(4000 || 8000, () => {
         console.log(`⚙️ Server is running at port : 4000`);
     })
 })
