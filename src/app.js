aa81675fbff74d93cdb34e26f4ac83772e57a1ba
Saurav Sharma <129518514+SHARMA-1729@users.noreply.github.com>
2024-10-06 21:33:59 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+     origin: "*",
     credentials:true,
-     methods: ['GET', 'POST', 'PUT', 'DELETE']
+    methods:["GET", "HEAD", "PUT", "POST", "DELETE","PATCH"],
 }))
 
 
