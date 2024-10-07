14462245a2771b0a1a1d15b11009ed10d83a4268
Saurav Sharma <129518514+SHARMA-1729@users.noreply.github.com>
2024-10-07 22:17:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from './db/index.js';
-import {app} from './app.js'
+import { app } from './app.js';
+
 dotenv.config({
     path: './.env'
-})
+});
 
-
-
+// Connect to MongoDB
 connectDB()
 .then(() => {
-    app.listen(4000 || 8000, () => {
+    app.listen(process.env.PORT || 8000, () => {
         console.log(`⚙️ Server is running at port : 4000`);
     })
 })
 .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
-})
+  });
+
+// Vercel will automatically handle the port, so export app
+export default app;
